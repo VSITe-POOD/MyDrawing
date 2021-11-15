bb1b538fd1742a1ace9129f15bd9ad78fa1b466f
Ante Žunić <anzunic@vsite.hr>
2021-11-15 19:07:07 +0100

    Resolved project skeleton

---
+++ Unit Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VSite.POOD.MyDrawing.Model
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

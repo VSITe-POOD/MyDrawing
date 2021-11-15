ffdf2cd2c06fa0a6f2176ab45e557ac107f82bda
Paolo Stabile <pastabil@vsite.hr>
2021-11-15 18:54:28 +0100

    Create projects skeleton

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vsite.Pood.MyDrawing.Model;
+
+namespace Vsite.Pood.MyDrawing.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

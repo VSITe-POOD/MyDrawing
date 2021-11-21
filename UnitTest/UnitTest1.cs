90e00008482171f5b9ebd8edbf276b58381646c4
Daniel Tošić <datosic@vsite.hr>
2021-11-21 20:38:29 +0100

    Create project skeletonvb

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vsite.Pood.MyDrawing.Model;
+namespace Vsite.Pood.MyDrawing.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

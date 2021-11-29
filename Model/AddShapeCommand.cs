4b6f627b5b91ebe5848e9568d63d3cd2f3f3d408
Paolo Stabile <pastabil@vsite.hr>
2021-11-29 19:34:29 +0100

    Implement AddShapeCommand

---
+++ Model/AddShapeCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Pood.MyDrawing.Model
+{
+    public class AddShapeCommand : ICommand
+    {
+        public AddShapeCommand(Drawing drawing, Shape shape)
+        {
+            this.drawing = drawing;
+            this.shape = shape;
+        }
+        public void Execute()
+        {
+            drawing.Add(shape);
+        }
+
+        public void Undo()
+        {
+            drawing.Remove(shape);
+        }
+
+        private Drawing drawing;
+        private Shape shape;
+    }
+}

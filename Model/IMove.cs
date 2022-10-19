8b2f0dd4f8746edde08dc0b93bdd73670a84c591
Julijan Sribar <julijan.sribar@gmail.com>
2022-10-19 21:09:50 +0200

    Create initial interfaces

---
+++ Model/IMove.cs
@@ -0,0 +1,7 @@
+﻿namespace Vsite.Pood.MyDrawing.Model
+{
+    public interface IMove
+    {
+        void Move(double dx, double dy);
+    }
+}
---
+++ Model/IResize.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Pood.MyDrawing.Model
+{
+    public enum Direction
+    {
+        North,
+        NorthEast,
+        East,
+        EastSouth,
+        South,
+        SouthWest,
+        West,
+        NorthWest
+    }
+    public interface IResize
+    {
+        void Resze(double dx, double dy, Direction direction);
+    }
+}
